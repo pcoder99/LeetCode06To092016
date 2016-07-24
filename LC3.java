b7a5357f36b9613a53ecf663e23f7fd4a290bb0f
pcoder99 <susan.2010hunter@gmail.com>
2016-07-23 22:13:52 -0700

    Update LC3.java

--- LC3.java
+++ LC3.java
@@ -1,1 +1,21 @@
+Problem Statement:
+  find the longest substring wihtout repeated characters
 
+  // whenever finding a repeated character, moving the left pointer right to the same character last found
+  public int longestsubstring(String s){
+    if(s.length() == 0){
+      return 0;
+    }
+    
+    HashMap<Character, Integer> map = new HashMap<>();
+    int max = 0;
+    for(int j = 0, i = 0; i < s.length(); ++i){
+      if(map.containsKey(s.charAt(i))){
+        j = Math.max(j, map.get(s.charAt(i)) + 1);
+      }
+      map.put(s.charAt(i),i);
+      max = Math.max(max, i - j + 1);
+    }
+    
+    return max;
+  }
