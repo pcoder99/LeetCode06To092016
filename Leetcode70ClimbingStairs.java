902643c27ade3b5ca767255a8a70217421ad4e1b
pcoder99 <susan.2010hunter@gmail.com>
2016-08-20 21:43:54 -0700

    Update Leetcode70ClimbingStairs.java

--- Leetcode70ClimbingStairs.java
+++ Leetcode70ClimbingStairs.java
@@ -23,3 +23,34 @@ public int ClimbStairs(int n){
   
   return ways[n];
 }
+
+Note: n element, the index is from 0 to n - 1, while we need the element 0, 1, ..., n
+totally, it would be n + 1 elements in the array.
+
+So, a better coding is to use variable instead of an array.
+If you draw a picture, it would be much easy to understand the idea
+
+0 -> 1
+1 -> 1  (one_step_before)
+2 -> 2  (two_step_before)
+3 -> 3
+
+public int func(int n){
+  if(n == 0 || n == 1){
+    return 1;
+  }
+  if(n == 2){
+    return 2;
+  }
+  
+  int one_step_before = 1;
+  int two_steps_before = 2;
+  int all_ways = 0;
+  for(int i = 3; i < n + 1; i++){
+    all_ways = one_step_before + two_steps_before;
+    one_step_before = two_steps_before;
+    two_steps_before = all_ways;
+  }
+  return all_ways;
+}
+}
