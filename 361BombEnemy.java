5db88ac4bb8c0dd22b1b040c47dee1dcc3135166
pcoder99 <susan.2010hunter@gmail.com>
2016-08-21 14:14:49 -0700

    Create 361BombEnemy.java

---
+++ 361BombEnemy.java
@@ -0,0 +1,10 @@
+Problem Statement
+  Given a 2D grid, each cell is either Enemy ("e"), or Wall ("w"), or empty ("0"), given a BOMB, find the most enemy it can kill.
+  A bomb can kill all the enemy in the same column and row until it hits the wall because the wall is too strong to destroy. 
+  Please not the bomb can only be putted on an empty cell. 
+
+Thinking
+  it looks like recursion problem
+
+Coding
+  
