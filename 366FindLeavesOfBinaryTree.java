9316e3dd7661929875287e72eda0896c4a42e5d0
pcoder99 <susan.2010hunter@gmail.com>
2016-08-28 19:08:17 -0700

    Update 366FindLeavesOfBinaryTree.java

--- 366FindLeavesOfBinaryTree.java
+++ 366FindLeavesOfBinaryTree.java
@@ -10,19 +10,20 @@ Idea:
 
 Implementation:
 
-  public List<List<Integer>> findLeaves(TreeNode root){
-    List<List<Integer> res = new LinkedList<>();
+  public class Solution {
+    public List<List<Integer>> findLeaves(TreeNode root) {
+         List<List<Integer>> res = new ArrayList<>();
     helper(root, res);
     return res;
-  }
-
-  public int helper(TreeNode root, List<List<Integer>> res){
+        
+    }
+    
+    public int helper(TreeNode root, List<List<Integer>> res){
     if(root == null) return -1;
-    int level = 1 + Math.max(height(root.left, res), height(root.right, res));
+    int level = 1 + Math.max(helper(root.left, res), helper(root.right, res));
     if(res.size() < (level + 1)) res.add(new ArrayList<>());
     res.get(level).add(root.val);
     return level;
     
   }
-
-  
+}
