8a08c6b57ed47cc066410d6653ccb051001e0799
pcoder99 <susan.2010hunter@gmail.com>
2016-09-02 22:15:35 -0700

    Create 5LongestPalindromicSubstring.java

---
+++ 5LongestPalindromicSubstring.java
@@ -0,0 +1,6 @@
+PS:
+  Given a string s, find the longest palindromic substring within S. You may assume the longest length of S is 1000, and there 
+  exists one unqiue longest palindromic substring.
+
+Think:
+  what is the concept of palindromic String ?
