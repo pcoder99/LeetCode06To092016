f909ed72d8dedbc5ea567ee48846bb421d336b35
pcoder99 <susan.2010hunter@gmail.com>
2016-09-06 20:51:11 -0700

    Update 14LongestCommonPrefix.java

--- 14LongestCommonPrefix.java
+++ 14LongestCommonPrefix.java
@@ -2,4 +2,17 @@ PS:
   Write a function to find the longest common prefix string amongst a string array
 
 Thought:
+  what is the difference between for and while loop
+  
+public String longestCommonPrefix(String[] strs) {
+        if(strs.length == 0)  return "";
+        
+        String pre = strs[0];
+        for(int i = 1; i < strs.length; i++){
+            while(strs[i].indexOf(pre) != 0){
+                pre = pre.substring(0, pre.length() - 1);
+            }
+        }
+        return pre;
+    }
   
