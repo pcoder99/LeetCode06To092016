86985d3f3392f5624c84f8dfa20e0188a62e68ab
pcoder99 <susan.2010hunter@gmail.com>
2016-09-07 20:54:24 -0700

    Create 38CountAndSay.java

---
+++ 38CountAndSay.java
@@ -0,0 +1,27 @@
+PS:
+  It is important to understand the 
+  
+  public String countAndSay(int n) {
+        StringBuilder cur = new StringBuilder("1");
+        StringBuilder prev;
+        int count;
+        char say;
+        
+        for(int i = 1; i < n; i++){
+            prev = cur;
+            cur = new StringBuilder();
+            say = prev.charAt(0);
+            count = 1;
+            for(int j = 1, len = prev.length(); j < len; j++){
+                if(prev.charAt(j) != say){
+                    cur.append(count).append(say);
+                    count = 1;
+                    say = prev.charAt(j);
+                }else
+                    count++;
+            }
+            cur.append(count).append(say);
+        }
+        return cur.toString();
+        
+    }
