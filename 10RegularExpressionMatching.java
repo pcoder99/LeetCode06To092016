f650b5bbcbbe72b63695f1728f98fb0b40f6087e
pcoder99 <susan.2010hunter@gmail.com>
2016-09-11 14:59:20 -0700

    Update 10RegularExpressionMatching.java

--- 10RegularExpressionMatching.java
+++ 10RegularExpressionMatching.java
@@ -2,4 +2,8 @@ PS: Implement Regular Expression Matching with support for "." and "*"
 
 Thought:  
   It is hard to match whatever it is.
+  Resolve existing problem based on subproblems solution.
+
+Implementation:
+
   
