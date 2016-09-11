bbcb006d9676c7b8262f5c78067bcf8e5e3577dd
pcoder99 <susan.2010hunter@gmail.com>
2016-09-10 21:14:15 -0700

    Update 28ImplementStrStr.java

--- 28ImplementStrStr.java
+++ 28ImplementStrStr.java
@@ -2,7 +2,18 @@ PS:
   Implement StrStr(). Return the index of the first occurrence of the needle in haystack.
 
 Thought:
-  KMP would be the best solution. 
+  KMP would be the best solution. The key point is returning index,  
 
 Implementation:
+
+public int strStr(String haystack, String needle) {
+        for(int i = 0; ; i++){
+            for(int j = 0; ;j++){
+                if(j == needle.length()) return i;
+                if(i + j == haystack.length()) return -1;
+                if(needle.charAt(j) != haystack.charAt(i + j)) break;
+            }
+        }
+        
+    }
   
