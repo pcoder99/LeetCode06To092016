76a6051e612a907f125b667c642f9df864d447d6
pcoder99 <susan.2010hunter@gmail.com>
2016-09-16 21:55:06 -0700

    Create 125ValidPalindrome.java

---
+++ 125ValidPalindrome.java
@@ -0,0 +1,38 @@
+PS:
+  Given a string, validate it is a palindrome. considering only alphanumeric character and ignore cases
+  
+Thought:
+  iterating from front/end to middle, O(n) solution
+  how to compare smallcase/uppercase, better leverage those Java Util method
+  
+
+Implementation:
+
+
+public class Solution {
+    public boolean isPalindrome(String s) {
+        if(s == null || s.length() == 0)
+          return true;
+        
+        int i = 0, j = s.length() - 1;
+        while( i < j ){
+            char b = s.charAt(i);
+            char e = s.charAt(j);
+            
+            if(!Character.isLetterOrDigit(b)){
+                i++;
+            }else if(!Character.isLetterOrDigit(e)){
+                j--;
+            }else{
+                if(Character.toLowerCase(b)!=Character.toLowerCase(e)){
+                    return false;
+                }
+                i++;
+                j--;
+            }
+            
+        }
+        return true;
+        
+    }
+}
