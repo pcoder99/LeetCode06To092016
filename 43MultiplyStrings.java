0c2a113cb8b215b0fe41c5eaa4fec707615679e3
pcoder99 <susan.2010hunter@gmail.com>
2016-09-19 18:16:26 -0700

    Create 43MultiplyStrings.java

---
+++ 43MultiplyStrings.java
@@ -0,0 +1,9 @@
+PS:
+  Given two integers represented as strings, return multiplication of the integers as a string.
+  
+  assuming: all are non-negative, the number can be arbitray large
+  how to find the position of the result is the key of the problem
+  
+  num1[i] * num2[j] will be persiste in i + j, i + j + 1. Actually, i + j is higher position and i + j + 1 is lower postion
+  
+  
