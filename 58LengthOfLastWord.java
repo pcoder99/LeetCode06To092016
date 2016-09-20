8c9533867d2734fdd33fc4c8342e1e90425a5154
pcoder99 <susan.2010hunter@gmail.com>
2016-09-20 07:41:00 -0700

    Update 58LengthOfLastWord.java

--- 58LengthOfLastWord.java
+++ 58LengthOfLastWord.java
@@ -1,1 +1,14 @@
+Given a string consisting of upper/lowr-alphabetic letter and empty space character, return the length of the last word
 
+take a look at the for loop
+
+public int lengthOfLastWord(String s) {
+        int len = 0, i = s.length() - 1;
+        for(; i >= 0 && s.charAt(i) == ' '; i--);
+        for(; i >= 0 && s.charAt(i) != ' '; i--){
+            len++;
+        }
+        
+        return len;
+        
+    }
