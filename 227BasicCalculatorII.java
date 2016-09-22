cf6359ca37f57dba76abd1db4115851a568c1cab
pcoder99 <susan.2010hunter@gmail.com>
2016-09-21 20:51:18 -0700

    Create 227BasicCalculatorII.java

---
+++ 227BasicCalculatorII.java
@@ -0,0 +1,9 @@
+PS:
+  Implement a basic calculator to evaluate a simple expression string. The string contains only non-negative integers, +, -, *, 
+  / and white spaces. The integer division should truncate toward zero.
+  
+  thought:
+    Need cover more cases.
+    
+  Implementation:
+    
