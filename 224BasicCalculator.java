d6ca5100c8122f2b46e9608b2270cc865cda0feb
pcoder99 <susan.2010hunter@gmail.com>
2016-09-21 20:44:42 -0700

    Update 224BasicCalculator.java

--- 224BasicCalculator.java
+++ 224BasicCalculator.java
@@ -1,1 +1,8 @@
+PS:
+  Implement a basic calculator to evaluate simple expression string. The simple expression string may contain open,
+  closing parenthesis, the plus or minus sign, non-negative integers and white space.
+  
+  Thought:
+    Stack is the data structure that will be used. How to make the code concise
+  
 
