3767e31755e25f2cf83c7fbc1361ba32d17c5558
pcoder99 <susan.2010hunter@gmail.com>
2016-10-15 13:31:58 -0700

    Update 227BasicCalculatorII.java

--- 227BasicCalculatorII.java
+++ 227BasicCalculatorII.java
@@ -3,7 +3,53 @@ PS:
   / and white spaces. The integer division should truncate toward zero.
   
   thought:
-    Need cover more cases.
+    No algorithm is needed. Just tedious code.
+    What will happen if it is out of band.
     
   Implementation:
+
+
+
+
     
+public class Solution {
+    public int calculate(String s) {
+        if(s==null || s.length() == 0){
+            return 0;
+        }
+        
+        int num = 0;
+        char sign = '+';
+        Stack<Integer> stack = new Stack<>();
+        for(int i = 0; i < s.length(); i++){
+            char c = s.charAt(i);
+            if( Character.isDigit(c) ){
+                num = num * 10 + Character.getNumericValue(c);
+            }
+            if(!Character.isDigit(c) && ' '!= c || i == s.length() - 1){
+                if(sign == '+'){
+                    stack.push(num);
+                }
+                if(sign == '*'){
+                    stack.push(stack.pop() * num);
+                }
+                if(sign == '-'){
+                    stack.push(-num);
+                }
+                if(sign == '/'){
+                    stack.push(stack.pop() / num);
+                }
+                
+                sign = c;
+                num = 0;
+            }
+        }
+        
+        int res = 0;
+        for(int i : stack){
+            res += i;
+        }
+        
+        return res;
+    }
+}
